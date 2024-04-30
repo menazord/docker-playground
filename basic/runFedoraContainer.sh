3c236e49a9b44f6502ead0cdf24bf3dacef56fdb
Cristian Meneses <cmeneses@gmail.com>
2024-04-29 18:51:43 -0700

    Added basic commands to start containers

---
+++ basic/runFedoraContainer.sh
@@ -0,0 +1,2 @@
+echo "Running Ubuntu (latest) on Docker..."
+docker container run --rm -ti docker.io/fedora:latest /bin/bash
---
+++ basic/runUbuntuContainer.sh
@@ -0,0 +1,2 @@
+echo "Running Ubuntu (latest) on Docker..."
+docker container run --rm -ti docker.io/ubuntu:latest /bin/bash
